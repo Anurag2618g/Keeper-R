5e776c07a225a327eb257ac43766072bc7a48b51
Anurag2618g <anurag2618g@gmail.com>
2024-09-01 19:27:25 +0530

    delete functionality added by passing function through component call

--- src/App.jsx
+++ src/App.jsx
@@ -14,12 +14,27 @@ function App() {
     });
     }
 
+    function deleteNote(id) {
+      setNotes( prev => {
+        return prev.filter((prev, index) => {
+          return index != id ;
+        })
+      })
+    }
+
   return (
     <div>
       <Header />
       <CreateArea onSave={saveElement} />
       {notes.map((note, index) => {
-        return <Note title={note.title} content={note.content} id={index} />;
+        return( 
+          <Note
+          key={index}
+          title={note.title} 
+          content={note.content} 
+          id={index} 
+          onDelete={deleteNote} />
+          );
       })}
       <Footer />
     </div>
